864f78b4531572ccdb9b7a4bede2ad1fb11e3aaa
galanlaurag <78027413+galanlaurag@users.noreply.github.com>
2023-03-13 15:03:26 +0000

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -90,27 +90,27 @@ drinks.click();
 }
 
 window.onload = function () {
-//     if (window.location.href.indexOf("#juices") > -1) {
-//        juices();
-//     } else if (window.location.href.indexOf("#shakes") > -1) {
-//         shakes();
-//     } else if (window.location.href.indexOf("#bowls") > -1) {
-//         bowls();
-//     } else if (window.location.href.indexOf("#savoury") > -1) {
-//         savoury();
-//     } else if (window.location.href.indexOf("#drinks") > -1) {
-//         drinks();
-//     }
+    if (window.location.href.indexOf("#juices") > -1) {
+       juices();
+    } else if (window.location.href.indexOf("#shakes") > -1) {
+        shakes();
+    } else if (window.location.href.indexOf("#bowls") > -1) {
+        bowls();
+    } else if (window.location.href.indexOf("#savoury") > -1) {
+        savoury();
+    } else if (window.location.href.indexOf("#drinks") > -1) {
+        drinks();
+    }
  
-    if ($(window).width() <= 576) {
-        $(".menu").addClass("animate__animated animate__fadeInDown");
-    }
+//     if ($(window).width() <= 576) {
+//         $(".menu").addClass("animate__animated animate__fadeInDown");
+//     }
 }
-window.onresize = function() {
-    if ($(window).width() <= 576) {
-        $(".menu").addClass("animate__animated animate__fadeInDown");
-    }
-}
+// window.onresize = function() {
+//     if ($(window).width() <= 576) {
+//         $(".menu").addClass("animate__animated animate__fadeInDown");
+//     }
+// }
 
 
 
